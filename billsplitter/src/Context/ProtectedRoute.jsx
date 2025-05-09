317353d0e6e38f500ee7537c5fe4d6df94248d5c
khushiiiii24 <khushisogani24@gmail.com>
2025-05-09 14:00:20 +0530

    sixth commit

--- billsplitter/src/Context/ProtectedRoute.jsx
+++ billsplitter/src/Context/ProtectedRoute.jsx
@@ -1,36 +1,38 @@
-import React, { createContext, useState, useEffect } from 'react';
-import { Navigate } from 'react-router-dom';
-import Instance from '../AxiosConfig';
-import { useAuth } from './AuthProvider';
+import React, { createContext, useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import Instance from "../AxiosConfig";
+import { useAuth } from "./AuthProvider";
 
 const AuthContext = createContext();
 
 function ProtectedRoute({ children }) {
-    const { isAuthenticated, setIsAuthenticated } = useAuth(); 
+  const { isAuthenticated, setIsAuthenticated } = useAuth();
 
-    useEffect(() => {
-        const checkAuth = async () => {
-            try {
-                const response = await Instance.get("/auth/checkToken");
-                if (response.status === 200) {
-                    setIsAuthenticated(true);
-                } else {
-                    setIsAuthenticated(false);
-                }
-            } catch (error) {
-                console.log("Error found:", error);
-                setIsAuthenticated(false);
-            }
-        };
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await Instance.get("/auth/checkToken", {
+          withCredentials: true,
+        });
+        if (response.status === 200) {
+          setIsAuthenticated(true);
+        } else {
+          setIsAuthenticated(false);
+        }
+      } catch (error) {
+        console.log("Error found:", error);
+        setIsAuthenticated(false);
+      }
+    };
 
-        checkAuth();
-    }, [setIsAuthenticated]); 
+    checkAuth();
+  }, [setIsAuthenticated]);
 
-    if (isAuthenticated === null) {
-        return <div>Loading...</div>; 
-    }
+  if (isAuthenticated === null) {
+    return <div>Loading...</div>;
+  }
 
-    return isAuthenticated ? children : <Navigate to="/" replace />;
+  return isAuthenticated ? children : <Navigate to="/" replace />;
 }
 
 export default ProtectedRoute;
